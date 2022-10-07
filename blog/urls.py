db8e7562d81676c00e6a8182eee5bf6023128e12
Matt <mjd014@live.co.uk>
2022-10-07 14:14:19 +0000

    added new path to urls via slug value.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     # Path to our PostList view, with template being 'index.html'.
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    # <slug:slug>/ captures URL value, in this case the slug value.
+    # First slug is the path convertor, second is a keyword.
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
