4d06e91bcb406cb8ffa1b2cf4923f53996997eff
Matt <mjd014@live.co.uk>
2022-10-07 13:40:18 +0000

    added new view for post details.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 # Create your views here.
@@ -17,3 +17,16 @@ class PostList(generic.ListView):
     # Here, we are limiting the amount of posts displayed to 6.
     # If posts go beyond 6, Django adds automated page navigation. 
     paginate_by = 6
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        # Filter posts by published ones only.
+        queryset = Post.objects.filter(status=1)
+        # Collects published post with correct slug.
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            
