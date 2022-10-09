b32a2730ca2888fe3efd07afcb5fc8535877b002
Matt <mjd014@live.co.uk>
2022-10-09 12:45:04 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+# This enables summernote editor during deployment.
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['codestar-mjd.herokuapp.com', 'localhost']
 
